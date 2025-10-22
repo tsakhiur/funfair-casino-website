1331f4f9f5235859617c8cc695df8615414c79a2
Tsahiur <2025239@student.cct.ie>
2025-10-22 23:58:47 +0100

    Update login.js
    Show alert if fields are empty, otherwise confirm successful login.

--- login.js
+++ login.js
@@ -3,3 +3,18 @@ function validateForm() {    // Function to validate the form fields
     // Get the value of the form fields
     var user = document.querySelector('input[type=text]').value; /*get text from username box*/
     var pass = document.querySelector('input[type=password]').value; /*get text from password box*/
+
+if (user == '' || pass == '') /*if one of field is empty*/
+
+    { 
+alert('Please fill out both fields!'); /*show message*/
+
+    return false; /*can't submit*/
+} else 
+    
+    {
+    alert('Successfully logged in!'); /*if filled both of them, show succ. looged in message*/
+}
+
+
+}
