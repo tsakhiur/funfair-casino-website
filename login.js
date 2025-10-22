23c96fda2f18c5448c82a49a2afb7fc6b2aa9b8b
Tsahiur <2025239@student.cct.ie>
2025-10-22 23:56:27 +0100

    Added login.js
    Created function to check form fields and get values from username and password boxes.

---
+++ login.js
@@ -0,0 +1,5 @@
+function validateForm() {    // Function to validate the form fields
+
+    // Get the value of the form fields
+    var user = document.querySelector('input[type=text]').value; /*get text from username box*/
+    var pass = document.querySelector('input[type=password]').value; /*get text from password box*/
