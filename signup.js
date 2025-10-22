1424cc4222b69e10d9c937e35fe8ca23f301a778
Tsahiur <2025239@student.cct.ie>
2025-10-23 00:01:09 +0100

    Added signup js
    Show success message and redirect user to login page after account creation.

---
+++ signup.js
@@ -0,0 +1,9 @@
+function validateForm() /* Function to validate the form fields*/
+
+{    
+
+alert('Successfully created account!'); /*shows message succ. created account*/
+location.href='login.html'; /*after clicking ok redirect to the login page*/
+return false; /*stops refesh*/
+
+}
